a2f38417b4145fe9336e9acd3422bd965c9d57bf
Padraig Gleeson <p.gleeson@gmail.com>
2016-12-20 15:44:31 +0000

    Adding more nml2 for testing & generated nestml scripts following changes in Juelich Code Gen meeting

---
+++ regenerate.sh
@@ -0,0 +1,20 @@
+set -e
+
+cd NeuroML2
+
+jnml LEMS_SimpleNet.xml -nest 
+
+jnml LEMS_NML2_Ex9_FN.xml -nest 
+
+jnml LEMS_2007One.xml -nest 
+
+jnml LEMS_Regular_HindmarshRose.xml -nest 
+
+rm *.json
+
+mv LEMS*.py *.nestml ../NESTML
+
+
+
+
+
